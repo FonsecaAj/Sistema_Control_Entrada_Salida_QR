5edf44013dc72dc18919ef0163a84a33185e9d1a
FonsecaAj <fonsecaajocs@gmail.com>
2025-11-14 10:40:31 -0600

    Evitar conflicto de carpetas vacías

---
+++ Sistema_Control_Entrada_Salida_QR/Pages/Modulo Usuarios/Registro Usuarios/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Sistema_Control_Entrada_Salida_QR.Pages.Modulo_Usuarios.Registro_Usuarios
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
