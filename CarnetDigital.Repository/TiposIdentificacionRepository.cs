60c6dad935561911686c34dbf28d8b197434b34e
J09Campos <julianj.camposaguilar@gmail.com>
2025-11-14 16:35:48 -0600

    Se Integra Funcionalidad e Interfaz del registro de funcionarios pendiente la validacion correcta de todos los campos y el envio de correo con la contraseña temporal.

---
+++ CarnetDigital.Repository/TiposIdentificacionRepository.cs
@@ -0,0 +1,36 @@
+﻿using CarnetDigital.Entities;
+using Dapper;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace CarnetDigital.Repository
+{
+    public class TiposIdentificacionRepository
+    {
+        private readonly IDbConnectionFactory _connectionFactory;
+
+        public TiposIdentificacionRepository(IDbConnectionFactory connectionFactory)
+        {
+            _connectionFactory = connectionFactory;
+        }
+
+        public IDbConnection CreateConnection()
+        {
+            return _connectionFactory.CreateConnection();
+        }
+
+        public async Task<IEnumerable<Tipos_Identificacion>> GetAllAsync()
+        {
+            using var conn = CreateConnection();
+
+            var lista = await conn.QueryAsync<Tipos_Identificacion>(
+                "sp_ListarTiposIdentificacion",
+                commandType: CommandType.StoredProcedure
+            );
+
+            return lista;
+        }
+    }
+}
+
--- Sistema_Control_Entrada_Salida_QR/Program.cs
+++ Sistema_Control_Entrada_Salida_QR/Program.cs
@@ -1,9 +1,16 @@
 using CarnetDigital.Repository;
+using CarnetDigital.Services;
+using CarnetDigital.Services.Abstract;
+using Sistema_Control_Entrada_Salida_QR.Services;
+using Sistema_Control_Entrada_Salida_QR.Services.Abstract;
+using Sistema_Control_Entrada_Salida_SQR.Services;
+using Sistema_Control_Entrada_Salida_SQR.Services.Abstract;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddHttpContextAccessor();
 
 
 // ---------------------------
@@ -11,6 +18,17 @@ builder.Services.AddRazorPages();
 // ---------------------------
 builder.Services.AddSingleton<IDbConnectionFactory, DbConnectionFactory>();
 
+builder.Services.AddScoped<FuncionariosRepository>();
+builder.Services.AddScoped<IFuncionariosService, FuncionariosService>();
+
+builder.Services.AddScoped<TiposIdentificacionRepository>();
+builder.Services.AddScoped<ITiposIdentificacionService, TiposIdentificacionService>();
+
+builder.Services.AddScoped<TiposFuncionarioRepository>();
+builder.Services.AddScoped<ITiposFuncionarioService, TiposFuncionarioService>();
+
+builder.Services.AddScoped<DependenciasRepository>();
+builder.Services.AddScoped<IDependenciasService, DependenciasService>();
 
 var app = builder.Build();
 
