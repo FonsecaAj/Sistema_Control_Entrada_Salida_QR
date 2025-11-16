c5b3a32e7e17b341d63452e4481b811a45cfef17
Jocs <fonsecaajocs@gmail.com>
2025-11-16 16:52:07 -0600

    Prueba a ver

--- CarnetDigital.Entities/Registros_Pendientes.cs
+++ CarnetDigital.Entities/Registros_Pendientes.cs
@@ -15,12 +15,11 @@ namespace CarnetDigital.Entities
         public string ID_Tipo_Identificacion { get; set; }
         public string Identificacion { get; set; }
         public DateTime Fecha_Nacimiento { get; set; }
-        public string Carrera { get; set; }
+        public string Id_Carrera { get; set; }
         public string Contrasena { get; set; }
         public byte[] Foto { get; set; }
         public DateTime Fecha_Registro { get; set; }
-        public string Estado { get; set; }
-        public string Mensaje { get; set; }
+        public string ID_Estado { get; set; }
 
     }
 }
--- Sistema_Control_Entrada_Salida_QR/Program.cs
+++ Sistema_Control_Entrada_Salida_QR/Program.cs
@@ -9,12 +9,87 @@ builder.Services.AddRazorPages();
 
 
 // ---------------------------
-// Conexi髇 a base de datos
+// Conexi锟絥 a base de datos
 // ---------------------------
 builder.Services.AddSingleton<IDbConnectionFactory, DbConnectionFactory>();
 
 builder.Services.AddScoped<Registro_PendientesRepository>();
 builder.Services.AddScoped<IRegistros_PendientesService, Registros_PendientesService>();
+builder.Services.AddScoped<FuncionariosRepository>();
+builder.Services.AddScoped<IFuncionariosService, FuncionariosService>();
+
+builder.Services.AddScoped<TiposIdentificacionRepository>();
+builder.Services.AddScoped<ITiposIdentificacionService, TiposIdentificacionService>();
+
+builder.Services.AddScoped<TiposFuncionarioRepository>();
+builder.Services.AddScoped<ITiposFuncionarioService, TiposFuncionarioService>();
+
+builder.Services.AddScoped<DependenciasRepository>();
+builder.Services.AddScoped<IDependenciasService, DependenciasService>();
+
+
+// ---------------------------
+// Generaci贸n de la credencial digital
+// ---------------------------
+
+// REPOSITORIO QR
+builder.Services.AddScoped<Credenciales_QRRepository>();
+builder.Services.AddScoped<ICredencialesQRServices, CredencialesQRServices>();
+// Inicio Sesi贸n
+
+builder.Services.AddScoped<UsuarioRepository>();
+builder.Services.AddScoped<IUsuarioService, UsuarioService>();
+
+// Registros Pendientes
+
+builder.Services.AddScoped<Registros_PendientesRepository>();
+builder.Services.AddScoped<IRegistros_PendientesService, Registros_PendientesService>();
+
+// Tipos Identificacion
+
+builder.Services.AddScoped<Tipos_IdentificacionRepository>();
+builder.Services.AddScoped<ITipos_IdentificacionService, Tipos_IdentificacionService>();
+
+// Carreras Programas
+
+builder.Services.AddScoped<Carreras_ProgramasRepository>();
+builder.Services.AddScoped<ICarreras_ProgramasService, Carreras_ProgramasService>();
+
+
+// Agregar soporte para cache distribuido y sesion
+
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Inicio Sesion/InicioSesion";   // Ruta cuando no est谩 autenticado
+        options.LogoutPath = "/Inicio Sesion/InicioSesion";  // Ruta al cerrar sesi贸n
+        options.ExpireTimeSpan = TimeSpan.FromMinutes(20);   // Tiempo de expiraci贸n de la cookie
+        options.SlidingExpiration = true;
+
+        options.Events = new CookieAuthenticationEvents
+        {
+            OnRedirectToLogin = ctx =>
+            {
+                var returnUrl = ctx.Request.Path;
+
+                if (ctx.Request.Cookies.ContainsKey(".AspNetCore.Cookies") &&
+                    (ctx.HttpContext.User?.Identity == null || !ctx.HttpContext.User.Identity.IsAuthenticated))
+                {
+                    ctx.Response.Redirect("/Inicio Sesion/InicioSesion?expired=true");
+                }
+                else
+                {
+                    ctx.Response.Redirect("/Inicio Sesion/InicioSesion?unauthenticated=true");
+                }
+
+                return Task.CompletedTask;
+            }
+        };
+    });
+
+
 
 var app = builder.Build();
 
